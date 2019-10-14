29f14f5fcf4cba5b54a3e181513abcead4eaf39f
Aldin Alic <aalic7@etf.unsa.ba>
2019-10-14 14:27:44 +0200

    Ispravljene greske u klasi Sat

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -2,16 +2,16 @@ package ba.unsa.etf.rpr;
 
 public class Sat {
 
-    static int sati, minute, sekunde;
+    int sati, minute, sekunde;
 
     public Sat(int sati, int minute, int sekunde) {
-        postavi(sati, minute, sekunde);
+        this.postavi(sati, minute, sekunde);
     }
 
-    public static void postavi(int sati, int minute, int sekunde) {
-        Sat.sati = sati;
-        Sat.minute = minute;
-        Sat.sekunde = sekunde;
+    public void postavi(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
     }
 
     public void sljedeci() {
